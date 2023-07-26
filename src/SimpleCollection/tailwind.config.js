702181866bc2826196cf51cdc60145d62b80850f
Adriano <adrianoalasia@gmail.com>
2023-07-26 17:32:06 -0400

    Add custom colors and progression

--- src/SimpleCollection/tailwind.config.js
+++ src/SimpleCollection/tailwind.config.js
@@ -1,7 +1,29 @@
 const path = require('path');
 
+function generateColor(name, percentage) {
+    return `color-mix(in srgb, var(--on-${name}), var(--${name}) ${percentage}%)`;
+}
+
+function generateProgression(name) {
+    let progression = {};
+    for (let i = 0; i <= 100; i += 5) {
+        progression[i] = generateColor(name, i);
+    }
+    return progression;
+}
+
 module.exports = {
     content: ['../*.{js,jsx}', '../**/*.js', path.join(path.dirname(require.resolve('@uniwebcms/express')), '**/*.js')],
     theme: {},
-    plugins: [require('@tailwindcss/line-clamp')]
+    plugins: [require('@tailwindcss/line-clamp')],
+    theme: {
+        extend: {
+            colors: {
+                primary: generateProgression('primary'),
+                secondary: generateProgression('secondary'),
+            }
+        }
+    }
+    
 };
+
